d4ffd619f9817e7d340abf97e21237c7b7389fc1
Thanhchu4i <chutienthanh444@gmail.com>
2024-10-27 19:27:42 +0700

    d

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getGames, getGameImage } = require('../controllers/getGames');
+const {getGameImage, getGamesWithUserInfo } = require('../controllers/getGames');
 const router = express.Router();
 
 // Endpoint để lấy danh sách game
-router.get('/games', getGames);
+router.get('/games', getGamesWithUserInfo);
 
 // Endpoint để lấy ảnh của game
 router.get('/games/image/:imageName', getGameImage);
