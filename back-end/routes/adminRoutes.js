fba8f15e42e5ad89be9ea37eca03912f952dac69
Thanhchu4i <chutienthanh444@gmail.com>
2024-10-31 13:45:25 +0700

    e

--- back-end/routes/adminRoutes.js
+++ back-end/routes/adminRoutes.js
@@ -5,5 +5,7 @@ const {getAdminStats} = require('../controllers/adminController');
 
 // Route để lấy tất cả các số liệu cho admin
 router.get('/stats', getAdminStats);
+// Route xóa người dùng
+router.delete('/', auth, userController.deleteUser);
 
 module.exports = router;
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -13,8 +13,6 @@ router.get('/', auth, userController.getUsers);
 // Route cập nhật thông tin người dùng
 router.put('/', auth, userController.updateUser);
 
-// Route xóa người dùng
-router.delete('/', auth, userController.deleteUser);
 // roure đổi mật khẩu
 router.post('/change-password', auth, userController.changePassword);
 
